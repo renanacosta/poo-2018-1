64a49cdbb6c4b27c62f44e2f7bdcb38a4c7c8608
Marcio Torres <marcio.torres@riogrande.ifrs.edu.br>
2018-02-19 23:56:14 -0300

    revisão

---
+++ 00-revisao/Revisao.java
@@ -0,0 +1,10 @@
+// todo programa Java deve ser "envolto" em uma Classe!
+class Revisao {
+  // snippet: digite "main" e pressione TAB
+  // todo programa deve ter um método principal (main)
+  public static void main(String[] args) {
+    // sequência de instruções:
+    // saída:
+    System.out.println("funciona!!!!");
+  }
+}
