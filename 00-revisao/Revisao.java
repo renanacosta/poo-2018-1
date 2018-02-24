2ffbce642ad038d57e58f609623379f35dc90cf6
Marcio Torres <marcio.torres@riogrande.ifrs.edu.br>
2018-02-24 12:39:40 -0300

    poo revisao explicacoes atualizadas

--- 00-revisao/Revisao.java
+++ 00-revisao/Revisao.java
@@ -26,29 +26,35 @@ class Revisao {
     // caractere: char (int)
     // boolean: true, false
     // tipos primitivos não podem ser null
+    // numéricos valem 0 por padrão enquanto boolean vale false
+    // long:
+    long a2 = 5000000000L; // long literal tem um "L" no fim
+    // booleanos:
     boolean d = true;
     System.out.println(d);
     char e = 'P';
     System.out.println(e);
-    // coerção
+    // coerção (coação) de um char para int
     int f = (int) e;
     System.out.println(f);
-    // conversão com perda
+    // conversão com perda, de double para int, os decimais são ignorados
     int g = (int) c;
     System.out.println(g);
     // tipos complexos/objetos (em oposição primitivos)
+    // por exemplo, string é um tipo complexo
     // strings são declaradas com aspas duplas
     String h = "programação"; // literal string
     System.out.println(h);
-    // .charAt(3) é um método de String
+    // .charAt(3) é um método de String (apenas complexos/objetos têm métodos)
     System.out.println(h.charAt(3)); // char 'g'
+    // tipos complexos podem ser nulos (são por padrão nulos)
     // null (nulo) é a ausência de valor
     String k = null;
     System.out.println(k);
-    // expressão aritmética
+    // expressão aritmética (seguem regras matemáticas)
     int l = (4 + 2) * 8;
     System.out.println(l);
-    // expressão booleana
+    // expressão booleana (seguem a álgebra booleana / ver tabela verdade)
     boolean m = true && true && true && false;
     System.out.println(m);
     boolean n = false || false || false || true;
@@ -56,15 +62,20 @@ class Revisao {
     // negação
     boolean o = !n;
     System.out.println(o);
+    // igualdade
+    o = 4 == 4;
+    System.out.println(o); // true, 4 é igual a 4
+    // diferença
+    o = 4 != 4;
+    System.out.println(o); // false, 4 não é diferente de 4
     // concatenando (+) string com
-    // qualquer outro tipos
-    // devolve string
+    // qualquer outro tipos devolve string
     String p = "poo" + o;
     System.out.println(p);
     // somar um literal inteiro com literal string
     // resulta em string
     System.out.println(4 + "4"); // 44
-    // todo if espera um expressão booleana
+    // todo "if" espera um expressão booleana entre parênteses
     if (a > 2) { // if significa "se"
       System.out.println("o a é maior que 2");
     } else { // significa "senão"
@@ -76,7 +87,7 @@ class Revisao {
     } else {
       System.out.println("n é falso");
     }
-    // if com vários else if
+    // if com vários "else if"
     if (c > 20) {
       System.out.println("c maior que 20");
     } else if (c > 10) {
@@ -101,39 +112,39 @@ class Revisao {
       case 3: {
         System.out.println("a é 3");
       }
-      default: { // equivalente ao "else" do if
+      default: { // equivalente ao "else" do if (ou outrocaso do visualg)
         System.out.println("a não é nem 1, nem 2, nem 3");
       }
     }
-    // if constante literal true
-    // entra sempre
+    // if constante literal "true" entra sempre
     if (true) { // chaves criam um bloco
       // cada bloco tem um ESCOPO
       // tudo que existe dentro do bloco
       int q = 0;
-      // q existe no bloco do "if"
+      // q existe apenas no bloco do "if"
       System.out.println(q);
       // a existe no bloco do main (escopo do main)
+      // logo existe no bloco do if também
       System.out.println(a);
     }
-    // q aqui está fora do escopo
-    // System.out.println(q);
+    // q aqui está fora do escopo, não existe no main
+    // System.out.println(q); // se descomentar essa linha ela não compila
 
     // laços! repetição
     int r = 2;
-    // sempre projetar uma condição de saída
+    // sempre projetar uma condição de saída!!!
     while (r < 10) {
       System.out.println(r);
-      r = r + 1; // r += 1; // r++;
+      r = r + 1; // ou "r += 1;" // ou "r++;"
     }
-    // for/para
+    // for/para   for(pré-execução e declarações;expressão booleana;pós-execução e alterações)
     for (int z = 2; z < 10; z = z + 1) {
       System.out.println(z);
     }
     // laço eterno (NÃO FAÇAM ISSO EM CASA)
     // while (true) { // for (;;;) {
     //   System.out.println("teste");
-    // }
+    // } // CTRL + C pára o programa se ficar "trancado" ou "preso" em um loop eterno
 
     // vetores (arrays)
     // [] é o símbolo do vetor (array)
@@ -145,23 +156,16 @@ class Revisao {
     // comprimento do vetor
     System.out.println(numeros.length);
     //  vetores tem tamanho fixo
-    // reatribuindo
+    // reatribuindo (não é uma inclusão)
     numeros = new int[]{3, 7, 9, 11};
     // atribuir "vazio"
     int[] pares = new int[4];
     System.out.println(pares[0]); // 0
+    // vetores de complexos/objetos têm posições nulas por padrão
     String[] nomes = new String[10];
     System.out.println(nomes[0]); // null
     nomes[0] = "";
     System.out.println(nomes[0]);
-
-
-
-
-
-
-
-
 
 
   }
